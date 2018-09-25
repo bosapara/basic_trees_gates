cc5f540ce30dc633af9aa7f875e876d065e5f15c
bosapara <38753015+bosapara@users.noreply.github.com>
2018-09-26 00:48:01 +0400

    gates

---
+++ init.lua
@@ -0,0 +1,55 @@
+doors.register_fencegate("doors:gate_sequoia", {
+	description = "Sequoia Fence Gate",
+	texture = "sequoia_wood.png",
+	material = "basic_trees:sequoia_planks",
+	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2}
+})
+
+doors.register_fencegate("doors:gate_birch", {
+	description = "Birch Fence Gate",
+	texture = "birch_wood.png",
+	material = "basic_trees:birch_planks",
+	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2}
+})
+
+doors.register_fencegate("doors:gate_cedar", {
+	description = "Cedar Fence Gate",
+	texture = "cedar_wood.png",
+	material = "basic_trees:cedar_planks",
+	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2}
+})
+
+doors.register_fencegate("doors:gate_cypress", {
+	description = "Cypress Fence Gate",
+	texture = "cypress_wood.png",
+	material = "basic_trees:cypress_planks",
+	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2}
+})
+
+doors.register_fencegate("doors:gate_eucalyptus", {
+	description = "Eucalyptus Fence Gate",
+	texture = "eucalyptus_wood.png",
+	material = "basic_trees:eucalyptus_planks",
+	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2}
+})
+
+doors.register_fencegate("doors:gate_maple", {
+	description = "Maple Fence Gate",
+	texture = "maple_wood.png",
+	material = "basic_trees:maple_planks",
+	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2}
+})
+
+doors.register_fencegate("doors:gate_oak", {
+	description = "Oak Fence Gate",
+	texture = "oak_wood.png",
+	material = "basic_trees:oak_planks",
+	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2}
+})
+
+doors.register_fencegate("doors:gate_willow", {
+	description = "Willow Fence Gate",
+	texture = "willow_wood.png",
+	material = "basic_trees:willow_planks",
+	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2}
+})
